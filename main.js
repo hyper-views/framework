8a89a328f7624c3f59b436def929057f4693019d
Erick Merchant <erickmerchant@gmail.com>
2016-08-17 00:36:52 -0700

    Work on simplicity

--- main.js
+++ main.js
@@ -1,77 +1,29 @@
 const catchLinks = require('catch-links')
 const singlePage = require('single-page')
-const loopOptions = {
-  create: require('virtual-dom/create-element'),
-  diff: require('virtual-dom/diff'),
-  patch: require('virtual-dom/patch')
-}
-const vdom = require('virtual-dom')
-const hyperx = require('hyperx')
-const hx = hyperx(vdom.h)
-const redux = require('redux')
-const thunk = require('redux-thunk').default
+const html = require('yo-yo')
 
-module.exports = function (settings) {
-  settings = Object.assign({
-    reducers: {},
-    middleware: [],
-    routes: {},
-    defaultComponent: function () {
-      return hx``
-    },
-    target: document.querySelector('body')
-  }, settings)
-
-  settings.reducers.context = contextReducer
-
-  settings.middleware.unshift(thunk)
-
-  const store = redux.createStore(redux.combineReducers(settings.reducers), redux.applyMiddleware(...settings.middleware))
-  const router = require('@erickmerchant/router')()
-  const dispatch = store.dispatch
-  const components = new Map()
-
-  Object.keys(settings.routes).forEach(function (route) {
-    components.set(route, settings.routes[route])
-
-    router.add(route)
-  })
-
+module.exports = function (store, component) {
   const show = singlePage(function (href) {
-    var context = router.match(href)
-
-    dispatch({type: 'SET_CONTEXT', context})
+    dispatch('context', href)
   })
 
-  catchLinks(window, show)
+  const el = component(store(), {dispatch, next, show, html})
 
-  const mainLoop = require('main-loop')
+  catchLinks(el, show)
 
-  const loop = mainLoop(store.getState(), function (state) {
-    let component = settings.defaultComponent
+  return el
 
-    if (state.context && components.has(state.context.route)) {
-      component = components.get(state.context.route)
-    }
+  function dispatch (type, data) {
+    render(store(type, data))
+  }
 
-    return component(state, {dispatch, next, show, hx})
-  }, loopOptions)
+  function render (state) {
+    const newEl = component(state, {dispatch, next, show, html})
 
-  store.subscribe(function () {
-    loop.update(store.getState())
-  })
-
-  settings.target.appendChild(loop.target)
+    html.update(el, newEl)
+  }
 }
 
 function next (callback) {
   process.nextTick(callback)
-}
-
-function contextReducer (state = null, action) {
-  if (action.type === 'SET_CONTEXT') {
-    return action.context
-  }
-
-  return state
 }
