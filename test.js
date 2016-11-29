fa78a317ccb82d6db72b5965a255c2978226208d
Erick Merchant <erickmerchant@gmail.com>
2016-11-28 23:04:53 -0800

    Break test cases up

--- test.js
+++ test.js
@@ -1,11 +1,10 @@
-var test = require('tape')
-var mockery = require('mockery')
+const test = require('tape')
+const mockery = require('mockery')
+const noop = function () {}
 
-test('test main', function (t) {
+test('test main - set-up', function (t) {
   let singlePage
   let theTarget = {}
-  let dispatchRun = false
-  let nextRun = false
 
   mockery.enable({
     useCleanCache: true,
@@ -13,9 +12,11 @@ test('test main', function (t) {
     warnOnUnregistered: false
   })
 
-  t.plan(12)
+  t.plan(4)
 
   mockery.registerMock('single-page', function (callback) {
+    t.ok(true)
+
     singlePage = callback
 
     return callback
@@ -32,28 +33,48 @@ test('test main', function (t) {
     store: function () {
       return {}
     },
+    component: noop,
+    diff: noop,
+    raf: noop
+  })(function (app) {
+    t.deepEqual(Object.keys(app), ['state', 'dispatch'])
+  })
+
+  mockery.disable()
+})
+
+test('test main - single-page > render', function (t) {
+  let singlePage
+  let theTarget = {}
+
+  mockery.enable({
+    useCleanCache: true,
+    warnOnReplace: false,
+    warnOnUnregistered: false
+  })
+
+  t.plan(5)
+
+  mockery.registerMock('single-page', function (callback) {
+    singlePage = callback
+
+    return callback
+  })
+
+  mockery.registerMock('catch-links', noop)
+
+  require('./main.js')({
+    target: theTarget,
+    store: function (state = {}) {
+      t.deepEqual({}, state)
+
+      return {}
+    },
     component: function (href) {
       t.equal(href, '/new-href')
 
       return function (app) {
         t.deepEqual(Object.keys(app), ['state', 'dispatch', 'show', 'next'])
-
-        if (!dispatchRun) {
-          dispatchRun = true
-
-          process.nextTick(function () {
-            app.dispatch()
-            app.dispatch()
-          }, 0)
-        }
-
-        if (!nextRun) {
-          nextRun = true
-
-          app.next(function (target) {
-            t.deepEqual(target, theTarget)
-          })
-        }
 
         return {}
       }
@@ -66,8 +87,112 @@ test('test main', function (t) {
     raf: function (callback) {
       process.nextTick(function () { callback() })
     }
-  })(function (app) {
-    t.deepEqual(Object.keys(app), ['state', 'dispatch'])
+  })
+
+  singlePage('/new-href')
+
+  mockery.disable()
+})
+
+test('test main - dispatch', function (t) {
+  let singlePage
+  let theTarget = {}
+  let dispatchRun = false
+
+  mockery.enable({
+    useCleanCache: true,
+    warnOnReplace: false,
+    warnOnUnregistered: false
+  })
+
+  t.plan(5)
+
+  mockery.registerMock('single-page', function (callback) {
+    singlePage = callback
+
+    return callback
+  })
+
+  mockery.registerMock('catch-links', noop)
+
+  require('./main.js')({
+    target: theTarget,
+    store: function (state = {}) {
+      t.ok(true)
+
+      return {}
+    },
+    component: function (href) {
+      return function (app) {
+        if (!dispatchRun) {
+          dispatchRun = true
+
+          process.nextTick(function () {
+            app.dispatch()
+            app.dispatch()
+          }, 0)
+        }
+
+        return {}
+      }
+    },
+    diff: function (target, newTarget) {
+      t.ok(true)
+    },
+    raf: function (callback) {
+      process.nextTick(function () { callback() })
+    }
+  })
+
+  singlePage('/new-href')
+
+  mockery.disable()
+})
+
+test('test main - next', function (t) {
+  let singlePage
+  let theTarget = {}
+  let nextRun = false
+
+  mockery.enable({
+    useCleanCache: true,
+    warnOnReplace: false,
+    warnOnUnregistered: false
+  })
+
+  t.plan(1)
+
+  mockery.registerMock('single-page', function (callback) {
+    singlePage = callback
+
+    return callback
+  })
+
+  mockery.registerMock('catch-links', noop)
+
+  require('./main.js')({
+    target: theTarget,
+    store: function (state = {}) {
+      return {}
+    },
+    component: function (href) {
+      return function (app) {
+        if (!nextRun) {
+          nextRun = true
+
+          app.next(function (target) {
+            t.deepEqual(target, theTarget)
+          })
+        }
+
+        return {}
+      }
+    },
+    diff: function (target, newTarget) {
+    },
+    raf: function (callback) {
+      process.nextTick(function () { callback() })
+    }
   })
 
   singlePage('/new-href')
