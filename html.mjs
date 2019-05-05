3bf4834258c528d937549ffc1cc7eeeeb43d025f
Erick Merchant <erickmerchant@gmail.com>
2019-05-05 11:52:55 -0700

    Don't use a class

--- html.mjs
+++ html.mjs
@@ -1,14 +1,10 @@
-class VNode {
-  constructor(tag, attributes, children) {
-    this.tag = tag
-
-    this.attributes = attributes
-
-    this.children = children.filter((child) => child != null)
+const create = (tag) => (attributes, ...children) => {
+  return {
+    tag,
+    attributes,
+    children: children.filter((child) => child != null)
   }
 }
-
-const create = (tag) => (attributes, ...children) => new VNode(tag, attributes, children)
 
 export default new Proxy({}, {
   get(_, tag) {
