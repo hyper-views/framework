3d1ac54f0e692586f0ed4ddc979141be4b427857
Erick Merchant <erickmerchant@gmail.com>
2020-05-20 20:55:09 -0400

    Formatting

--- fixtures/component.mjs
+++ fixtures/component.mjs
@@ -1,20 +1,22 @@
 import {html} from '../main.mjs'
 
-export const component = (state) => html`<main>
-  <h1>${state.heading}</h1>
-  <img src=${state.src} />
-  <button onclick=${state.onclick} type="button">Approve</button>
-  ${
-    state.hasP
-    ? html`<p ${{class: state.isRed ? 'red' : 'blue'}}>
-        ${state.pText1} ${state.pText2} ${state.pText3}
-      </p>`
-    : null
-  } ${
-    state.hasSvg
-    ? html`<svg>
-        <path d=${state.svgPath} />
-      </svg>`
-    : null
-  }
-</main>`
+export const component = (state) => html`
+  <main>
+    <h1>${state.heading}</h1>
+    <img src=${state.src} />
+    <button onclick=${state.onclick} type="button">Approve</button>
+    ${
+      state.hasP
+      ? html`<p ${{class: state.isRed ? 'red' : 'blue'}}>
+          ${state.pText1} ${state.pText2} ${state.pText3}
+        </p>`
+      : null
+    } ${
+      state.hasSvg
+      ? html`<svg>
+          <path d=${state.svgPath} />
+        </svg>`
+      : null
+    }
+  </main>
+`
