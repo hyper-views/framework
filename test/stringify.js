01e491a764f02de99c2cca2288ae90e99e10a22b
Erick Merchant <erickmerchant@gmail.com>
2020-06-29 21:43:08 -0400

    Test stringify

---
+++ test/stringify.js
@@ -0,0 +1,28 @@
+import test from 'ava'
+import {stringify} from '../stringify.js'
+import {html} from '../main.js'
+
+test('stringify', (t) => {
+  t.deepEqual(
+    stringify(html`
+      <form action=${'/submit'}>
+        <fieldset>
+          <legend>Very Important ${'Values'}</legend>
+          ${[1, 2, 3].map(
+            (i) => html`
+              <input
+                required
+                id=${`value${i}`}
+                name=${`value${i}`}
+                type="input"
+                ${{placeholder: 'Add A Value'}}
+              />
+            `
+          )}
+        </fieldset>
+        <button type="button" onclick=${() => {}}>Submit</button>
+      </form>
+    `),
+    '<form action="/submit"><fieldset><legend>Very Important Values</legend><input required id="value1" name="value1" type="input" placeholder="Add A Value"><input required id="value2" name="value2" type="input" placeholder="Add A Value"><input required id="value3" name="value3" type="input" placeholder="Add A Value"></fieldset><button type="button">Submit</button></form>'
+  )
+})
